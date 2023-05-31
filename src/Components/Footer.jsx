a7c02e9ef240a89ad6bdc1fa60d530bcb45135b0
Shubham <aryan100jangid@gmail.com>
2023-05-31 23:08:58 +0530

    Make footer bg white

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
-            <div className='p-2 bg-[#e1b4e5] flex flex-col items-center'>
+            <div className='p-2 bg-[white] flex flex-col items-center'>
                 <p className='text-black p-2'>Copyright© 2023 Shubham | Designed and Coded by Shubham. For Contact email me at<a href="mailto:aryan10jangid@gmail.com">aryan10jangid@gmail.com</a></p>
             </div>
         </>
