d1cf6aacfa00b95def93fe290fd328c374526261
Shubham <aryan100jangid@gmail.com>
2023-06-01 19:08:33 +0530

    Fix colors

--- src/Components/Projectdetails.jsx
+++ src/Components/Projectdetails.jsx
@@ -8,10 +8,10 @@ const Projectdetails = ({title,d1,d2,d3,img,site,github}) => {
             <div className='mt-10 text-semibold text-[#eeb4aa] text-[1.4rem]'>
                 {title}
             </div>
-            <div className='w-[40rem] text-[1.2rem] text-black md:w-full'>
-                <p className='my-4 w-[30rem] md:w-[50rem] p-1 md:p-0'>{d1}</p>
-                <p className='my-4 w-[30rem] md:w-[50rem] p-1 md:p-0'>{d3}</p>
-                <p className='my-4 w-[30rem] md:w-[50rem] p-1 md:p-0'>{d2}</p>
+            <div className='w-[40rem] text-[1.2rem] md:w-full'>
+                <p className='my-4 w-[30rem] md:w-[50rem] p-1 text-white md:p-0'>{d1}</p>
+                <p className='my-4 w-[30rem] md:w-[50rem] p-1 text-white  md:p-0'>{d3}</p>
+                <p className='my-4 w-[30rem] md:w-[50rem] p-1 text-white  md:p-0'>{d2}</p>
                 <Techs img={img}/>
                 <Button site={site} github={github}/>
             </div>
