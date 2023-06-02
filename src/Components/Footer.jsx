6b743b00041db87f19a115251e82181bb5610c3b
Shubham <aryan100jangid@gmail.com>
2023-06-02 10:24:57 +0530

    Fix footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
             <div className='p-2 bg-[white] flex flex-col items-center'>
-                <p className='text-black p-2'><b>Copyright© 2023 | Design & Coded by Shubham</b></p>
+                <p className='text-black p-2'>Copyright© 2023 | Designed and Coded by Shubham</p>
             </div>
         </>
     );
